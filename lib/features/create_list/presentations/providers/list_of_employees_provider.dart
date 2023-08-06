1042f447f33c5359af1e046e96ca0de7e0d77fec
abdalmouneim <mo.m.eg21@gmail.com>
2023-08-06 06:44:23 +0300

    toast in create list

--- lib/features/create_list/presentations/providers/list_of_employees_provider.dart
+++ lib/features/create_list/presentations/providers/list_of_employees_provider.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:workhours/common/services/navigation_services.dart';
 import 'package:workhours/common/services/pdf/pdf_genrate_api.dart';
+import 'package:workhours/common/utils/utils.dart';
 import 'package:workhours/features/home/data/model/employee_model.dart';
+import 'package:workhours/generated/l10n.dart';
 
 class ListOfEmployeesProvider extends ChangeNotifier {
   final BuildContext _context = NavigationService.context;
@@ -17,13 +21,13 @@ class ListOfEmployeesProvider extends ChangeNotifier {
     print("----------------------");
     try {
       Future.delayed(
-        Duration(seconds: 3),
+        Duration(seconds: 2),
         () async => await PdfGenerateApi.generate(EmployeeModel()).then(
-          (value) {
+          (File value) {
             print(value);
-
             isLoadSave = false;
             notifyListeners();
+            Utils.showSuccess(S.of(_context).pdfFileSavedAtPath(value.path));
           },
         ),
       );
@@ -42,8 +46,10 @@ class ListOfEmployeesProvider extends ChangeNotifier {
       Future.delayed(
         Duration(seconds: 2),
         () async => await PdfGenerateApi.generate(EmployeeModel())
-            .then((value) async => await Share.shareFiles([value.path]))
-            .then((value) {
+            .then((File file) async {
+          await Share.shareFiles([file.path]);
+          Utils.showSuccess(S.of(_context).pdfFileSavedAtPath(file.path));
+        }).then((value) {
           isLoadShare = false;
           notifyListeners();
         }),
